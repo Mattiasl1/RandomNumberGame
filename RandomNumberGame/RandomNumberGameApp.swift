9d8111c59ba700a4b79c1ddddc1a3b1c4b2e981e
Mattias Liljenberg <liljenbeerg@hotmail.com>
2021-12-08 15:01:37 +0100

    Initial Commit

---
+++ RandomNumberGame/RandomNumberGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomNumberGameApp.swift
+//  RandomNumberGame
+//
+//  Created by Mattias Liljenberg on 2021-12-08.
+//
+
+import SwiftUI
+
+@main
+struct RandomNumberGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
